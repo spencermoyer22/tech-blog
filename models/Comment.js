4a6f8847ed4ef18d9fa69b3b4f13d91d46f0eae5
Big Head <spencer.moyer16@yahoo.com>
2021-05-09 15:59:35 -0700

    created comment model and index file for models

---
+++ models/Comment.js
@@ -0,0 +1,45 @@
+const {Model, DataTypes} = require('sequelize');
+const {User, Post} = require('./');
+
+class Comment extends Model{}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: User,
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Post,
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment;
